fdf579e5badb27b85a676df353d68dd9192299a6
Emma Bishop <bishemma1@gmail.com>
2024-04-15 13:12:10 -0700

    Initial commit of setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='tcrconverter',
+      version='0.1',
+      author='Emma Bishop',
+      author_email='emmab5@uw.edu',
+      license='MIT',
+      install_requires=['pandas'])
