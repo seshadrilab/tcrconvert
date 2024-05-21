a856638be726b7060606b5934af9cdba55065d24
Emma Bishop <bishemma1@gmail.com>
2024-05-20 18:10:43 -0700

    Add tests for more conversion parameter combos

--- tcrconverter/tests/test_convert.py
+++ tcrconverter/tests/test_convert.py
@@ -22,6 +22,10 @@ custom_df = pd.DataFrame({'myV': ['TRAV1-2*01', 'TRBV6-1*01'],
                           'myJ': ['TRAJ12*01', 'TRBJ2-1*01'],
                           'myCDR3': ['CAVMDSSYKLIF', 'CASSGLAGGYNEQFF']})
 
+extract_df = pd.DataFrame({'v_gene': ['TCRAV01-02*01', 'TCRBV06-01*01'],
+                           'j_gene': ['TCRAJ12*01', 'TCRBJ02-01*01'],
+                           'cdr3': ['CAVMDSSYKLIF', 'CASSGLAGGYNEQFF']})
+
 
 @pytest.mark.parametrize('df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols,out', [
     (tenx_df,'tenx','adaptive',None,False,True,adapt_df),
@@ -29,8 +33,9 @@ custom_df = pd.DataFrame({'myV': ['TRAV1-2*01', 'TRBV6-1*01'],
     (adapt_df,'adaptive','tenx',None,False,True,tenx_df),
     (adapt_v2_df,'adaptive_v2','tenx',None,False,True,tenx_df),
     (custom_df,'imgt','tenx',['myV', 'myJ', 'myCDR3'],False,True,tenx_df),
-    (imgt_df,'imgt','adaptive',None,False,True,adapt_df)])
+    (imgt_df,'imgt','adaptive',None,False,True,adapt_df),
+    (adapt_v2_df,'adaptive','tenx',None,False,True,tenx_df),
+    (tenx_df,'tenx','adaptive',None,True,False,extract_df)])
 def test_convert_tcr(df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols,out):
     result = convert.convert_tcr(df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols)
     pd.testing.assert_frame_equal(result, out)
-
