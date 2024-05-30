8e3f722820ff94119bec301efe2d084c803747b8
Emma Bishop <bishemma1@gmail.com>
2024-05-30 00:10:18 -0700

    Add package data to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='tcrconverter',
       version='0.1',
       author='Emma Bishop',
       author_email='emmab5@uw.edu',
       license='MIT',
+      packages=find_packages(),
+      package_data={"": ["*.csv", "*.fa"]},
       install_requires=['pandas'])
