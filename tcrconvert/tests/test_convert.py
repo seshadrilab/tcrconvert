cf6f1eb7186ed80d3f6f208d495e073f100ce474
Emma Bishop <bishemma1@gmail.com>
2024-08-15 17:02:15 -0700

    Add comments describing tests and some new tests

--- tcrconvert/tests/test_convert.py
+++ tcrconvert/tests/test_convert.py
@@ -28,13 +28,24 @@ extract_df = pd.DataFrame({'v_gene': ['TCRAV01-02*01', 'TCRBV06-01*01'],
 
 
 @pytest.mark.parametrize('df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols,out', [
+    # 10X <-> Adaptive
     (tenx_df,'tenx','adaptive',None,False,True,adapt_df),
     (tenx_df,'tenx','adaptive_v2',None,False,True,adapt_v2_df),
     (adapt_df,'adaptive','tenx',None,False,True,tenx_df),
     (adapt_v2_df,'adaptive_v2','tenx',None,False,True,tenx_df),
-    (custom_df,'imgt','tenx',['myV', 'myJ', 'myCDR3'],False,True,tenx_df),
+    # Adaptive <-> IMGT
+    # (adapt_df,'adaptive','imgt',None,False,True,imgt_df),  # KeyError: 'imgt'
+    # (adapt_v2_df,'adaptive_v2','imgt',None,False,True,imgt_df),  # KeyError: 'imgt'
     (imgt_df,'imgt','adaptive',None,False,True,adapt_df),
+    (imgt_df,'imgt','adaptive_v2',None,False,True,adapt_v2_df),
+    # IMGT <-> 10X
+    # (tenx_df,'tenx','imgt',None,False,True,imgt_df),  # KeyError: 'imgt'
+    (imgt_df,'imgt','tenx',None,False,True,tenx_df),
+    # Custom column names
+    (custom_df,'imgt','tenx',['myV', 'myJ', 'myCDR3'],False,True,tenx_df),
+    # Adaptive V2 format but user inputs "adaptive"
     (adapt_v2_df,'adaptive','tenx',None,False,True,tenx_df),
+    # Extract TCRs only
     (tenx_df,'tenx','adaptive',None,True,False,extract_df)])
 def test_convert_tcr(df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols,out):
     result = convert.convert_tcr(df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols)
