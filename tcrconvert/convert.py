993a202a9afb4d7636229e7c73a81f7429d171b1
Emma Bishop <bishemma1@gmail.com>
2024-08-20 19:21:40 -0700

    Refactor convert_tcr and add new convert functions
    - Rename convert_tcr to convert_df and rename arguments to be more
    intuitive
    - Add convert_file function to convert files instead of dataframes
    - Add load_convert function to convert file as it's loaded into
    dataframe

--- tcrconvert/convert.py
+++ tcrconvert/convert.py
@@ -8,81 +8,83 @@ col_ref = {'tenx': ('v_gene', 'j_gene', 'cdr3'),
            'imgt': ('v_gene', 'j_gene', 'cdr3')}
 
 
-def convert_tcr(df,
-                fmt_from,  # ['tenx', 'adaptive', 'adaptive_v2', 'imgt']
-                fmt_to,  # ['tenx', 'adaptive', 'adaptive_v2', 'imgt']
-                cols_use=None, extract_tcr=False, convert_cols=False):
+def convert_df(df,
+                frm,  # ['tenx', 'adaptive', 'adaptive_v2', 'imgt']
+                to,  # ['tenx', 'adaptive', 'adaptive_v2', 'imgt']
+                rename_cols=True,
+                frm_cols=None,
+                species='human'):
     '''Convert TCR gene names from one naming convention to another.
 
     :param df: Dataframe of TCRs
     :type df: dataframe
-    :param fmt_from: Starting format of TCR data
-    :type fmt_from: str
-    :param fmt_to: Format to convert TCR data to
-    :type fmt_to: str
-    :param cols_use: List or tuple of current V, J, and CDR3 column names
-    :type cols_use: list or tuple, optional
-    :param extract_tcr: Whether to return only the V, J, and CDR3 columns
-    :type extract_tcr: bool, optional
-    :param convert_cols: Whether to convert column names to match new format
-    :type convert_cols: bool, optional
+    :param frm: Starting format of TCR data
+    :type frm: str
+    :param to: Format to convert TCR data to
+    :type to: str
+    :param rename_cols: Whether to convert column names to match new format
+    :type rename_cols: bool, optional
+    :param frm_cols: List or tuple of current V, J, and CDR3 column names
+    :type frm_cols: list or tuple, optional
+    :param species: Species data folder to use
+    :type species: str, optional
     :return: Converted TCR data
     :rtype: dataframe
     '''
 
     # Determine if we're using the 10X lookup table
-    if fmt_from == 'tenx':
-        lookup_f = files('tcrconvert.data').joinpath('lookup_from_tenx.csv')
+    if frm == 'tenx':
+        lookup_f = files('tcrconvert') / 'data' / species / 'lookup_from_tenx.csv'
         print("CONVERTING FROM 10X: CHOOSING *01 AS ALLELE FOR ALL GENES")
     else:
-        lookup_f = files('tcrconvert.data').joinpath('lookup.csv')
+        lookup_f = files('tcrconvert') / 'data' / species / 'lookup.csv'
 
     # Load lookup table
     try:
         lookup = pd.read_csv(lookup_f)
     except FileNotFoundError:
-        print('Lookup table not found, please download IMGT reference FASTAs and run build_lookup.py')
+        print('Lookup table not found, please download IMGT reference FASTAs and run build_lookup_from_fastas()')
 
     # Figure out columns to use
-    if fmt_from == 'adaptive' and 'vMaxResolved' in df.columns:
+    if frm == 'adaptive' and 'vMaxResolved' in df.columns:
         cols_from = 'adaptive_v2'
-    elif fmt_from == 'imgt' and not cols_use:
+    elif frm == 'imgt' and not frm_cols:
         print('No column names provided for IMGT data, will use 10X column names:\n' 
                       + str(col_ref['tenx']))
         cols_from = 'tenx'
     else:
-        cols_from = fmt_from
+        cols_from = frm
 
     # Column names to use for input and output
     # TODO: What if converting to imgt and want to update columns?
-    if cols_use:
-        v_from = cols_use[0]
-        j_from = cols_use[1]
-        cdr3_from = cols_use[2]
+    if frm_cols:
+        v_from = frm_cols[0]
+        j_from = frm_cols[1]
+        cdr3_from = frm_cols[2]
     else:
         v_from = col_ref[cols_from][0]
         j_from = col_ref[cols_from][1]
         cdr3_from = col_ref[cols_from][2]
 
-    if convert_cols:
-        v_to = col_ref[fmt_to][0]
-        j_to = col_ref[fmt_to][1]
-        cdr3_to = col_ref[fmt_to][2]
+    if rename_cols:
+        v_to = col_ref[to][0]
+        j_to = col_ref[to][1]
+        cdr3_to = col_ref[to][2]
     else:
         v_to = v_from
         j_to = j_from
         cdr3_to = cdr3_from
 
     # Convert V and J genes
-    v_genes = df[[v_from]].merge(lookup[[fmt_from, fmt_to]], how='left', 
-                                 left_on=v_from, right_on=fmt_from).\
-        drop(columns=fmt_from)
-    v_genes_bad = v_genes[v_genes[fmt_to].isna()][v_from].tolist()
+    v_genes = df[[v_from]].merge(lookup[[frm, to]], how='left',
+                                 left_on=v_from, right_on=frm).\
+        drop(columns=frm)
+    v_genes_bad = v_genes[v_genes[to].isna()][v_from].tolist()
 
-    j_genes = df[[j_from]].merge(lookup[[fmt_from, fmt_to]], how='left', 
-                                 left_on=j_from, right_on=fmt_from).\
-        drop(columns=fmt_from)
-    j_genes_bad = j_genes[j_genes[fmt_to].isna()][j_from].tolist()
+    j_genes = df[[j_from]].merge(lookup[[frm, to]], how='left',
+                                 left_on=j_from, right_on=frm).\
+        drop(columns=frm)
+    j_genes_bad = j_genes[j_genes[to].isna()][j_from].tolist()
 
     # Display genes we couldn't convert
     # TODO: have option to keep mangled gene names
@@ -93,18 +95,112 @@ def convert_tcr(df,
 
     # Consolidate converted data
     out = pd.DataFrame()
-    out[v_to] = v_genes[fmt_to]
-    out[j_to] = j_genes[fmt_to]
+    out[v_to] = v_genes[to]
+    out[j_to] = j_genes[to]
     out[cdr3_to] = df[cdr3_from]
 
-    if extract_tcr:
-        return out
+    # Swap out data in original dataframe
+    out_df = df.copy()
+    out_df[v_from] = out[v_to].values
+    out_df[j_from] = out[j_to].values
+    out_df[cdr3_from] = out[cdr3_to].values
+    out_df.rename(columns={v_from: v_to,
+                            j_from: j_to,
+                            cdr3_from: cdr3_to}, inplace = True)
+    return out_df
+
+
+def convert_file(infile,
+                 outfile,
+                 frm,  # ['tenx', 'adaptive', 'adaptive_v2', 'imgt']
+                 to,  # ['tenx', 'adaptive', 'adaptive_v2', 'imgt']
+                 rename_cols=True,
+                 frm_cols=None,
+                 species='human'):
+    '''Convert file with TCR data from one naming convention to another.
+
+    :param infile: Path to input file (e.g. '~/Downloads/indata.csv')
+    :type infile: str
+    :param outfile: Path for output file (e.g. '~/Downloads/outdata.tsv')
+    :type outfile: str
+    :param df: Dataframe of TCRs
+    :type df: dataframe
+    :param frm: Starting format of TCR data
+    :type frm: str
+    :param to: Format to convert TCR data to
+    :type to: str
+    :param rename_cols: Whether to convert column names to match new format
+    :type rename_cols: bool, optional
+    :param frm_cols: List or tuple of current V, J, and CDR3 column names
+    :type frm_cols: list or tuple, optional
+    :param species: Species data folder to use
+    :type species: str, optional
+    :return: Converted TCR data
+    :rtype: dataframe
+    '''
+    # Load input
+    if infile.endswith('.csv'):
+        dat = pd.read_csv(infile)
+    elif infile.endswith('.tsv'):
+        dat = pd.read_csv(infile, sep='\t')
+    elif infile.endswith('.xls') or infile.endswith('.xlsx') or infile.endswith('.ods'):
+        dat = pd.read_excel(infile)
     else:
-        out_df = df.copy()
-        out_df[v_from] = out[v_to].values
-        out_df[j_from] = out[j_to].values
-        out_df[cdr3_from] = out[cdr3_to].values
-        out_df.rename(columns={v_from: v_to, 
-                               j_from: j_to,
-                               cdr3_from: cdr3_to}, inplace = True)
-        return out_df
+        print('Please input a CSV, TSV, Excel, or ODF Spreadsheet file')
+
+    # Convert
+    out_dat = convert_df(dat, frm=frm, to=to, rename_cols=rename_cols,
+                         frm_cols=frm_cols, species=species)
+
+    # Save output
+    if outfile.endswith('.csv'):
+        out_dat.to_csv(outfile, index=False)
+    elif outfile.endswith('.tsv'):
+        out_dat.to_csv(outfile, index=False, sep='\t')
+    elif outfile.endswith('.xls') or outfile.endswith('.xlsx') or outfile.endswith('.ods'):
+        out_dat.to_excel(outfile, index=False)
+    else:
+        print('Please use a CSV, TSV, Excel, or ODF Spreadsheet file as output.')
+
+
+def load_convert(infile,
+                 frm,  # ['tenx', 'adaptive', 'adaptive_v2', 'imgt']
+                 to,  # ['tenx', 'adaptive', 'adaptive_v2', 'imgt']
+                 rename_cols=True,
+                 frm_cols=None,
+                 species='human'):
+    '''Load a file with TCR data and convert from one naming convention to another.
+
+    :param infile: Path to input file (e.g. '~/Downloads/indata.csv')
+    :type infile: str
+    :param df: Dataframe of TCRs
+    :type df: dataframe
+    :param frm: Starting format of TCR data
+    :type frm: str
+    :param to: Format to convert TCR data to
+    :type to: str
+    :param rename_cols: Whether to convert column names to match new format
+    :type rename_cols: bool, optional
+    :param frm_cols: List or tuple of current V, J, and CDR3 column names
+    :type frm_cols: list or tuple, optional
+    :param species: Species data folder to use
+    :type species: str, optional
+    :return: Converted TCR data
+    :rtype: dataframe
+    '''
+
+    # Load input
+    if infile.endswith('.csv'):
+        dat = pd.read_csv(infile)
+    elif infile.endswith('.tsv'):
+        dat = pd.read_csv(infile, sep='\t')
+    elif infile.endswith('.xls') or infile.endswith('.xlsx') or infile.endswith('.ods'):
+        dat = pd.read_excel(infile)
+    else:
+        print('Please input a CSV, TSV, Excel, or ODF Spreadsheet file')
+
+    # Convert
+    out_dat = convert_df(dat, frm=frm, to=to, rename_cols=rename_cols,
+                         frm_cols=frm_cols, species=species)
+
+    return(out_dat)
--- tcrconvert/tests/test_convert.py
+++ tcrconvert/tests/test_convert.py
@@ -1,5 +1,6 @@
 import pytest
 import pandas as pd
+from importlib.resources import files
 import tcrconvert
 
 imgt_df = pd.DataFrame({'v_gene': ['TRAV1-2*01', 'TRBV6-1*01'],
@@ -26,27 +27,43 @@ extract_df = pd.DataFrame({'v_gene': ['TCRAV01-02*01', 'TCRBV06-01*01'],
                            'j_gene': ['TCRAJ12*01', 'TCRBJ02-01*01'],
                            'cdr3': ['CAVMDSSYKLIF', 'CASSGLAGGYNEQFF']})
 
+example_adaptive = pd.DataFrame([['AAACCTGAGACCACGA-1', True, 'AAACCTGAGACCACGA-1_contig_1', True, 521, 'TRA', 'TCRAV01-02*01', 'TRBD1', 'TCRAJ12*01', 'TRAC', True, True, 'CAVMDSSYKLIF', 'TGTGCTGTGATGGATAGCAGCTATAAATTGATCTTC', 1569, 2, 'clonotype16', 'clonotype16_consensus_1'],
+                                 ['AAACCTGAGACCACGA-1', True, 'AAACCTGAGACCACGA-1_contig_3', True, 584, 'TRB', 'TCRBV06-01*01', 'TRBD2', 'TCRBJ02-01*01', 'TRBC2', True, True, 'CASSGLAGGYNEQFF', 'TGTGCCAGCAGTGGACTAGCGGGGGGCTACAATGAGCAGTTCTTC', 5238, 7, 'clonotype16', 'clonotype16_consensus_2'],
+                                 ['AAACCTGAGGCTCTTA-1', True, 'AAACCTGAGGCTCTTA-1_contig_1', True, 551, 'TRB', 'TCRBV06-04*01', 'TRBD2', 'TCRBJ02-03*01', 'TRBC2', True, True, 'CASSGVAGGTDTQYF', 'TGTGCCAGCAGTGGGGTAGCGGGAGGCACAGATACGCAGTATTTT', 3846, 4, 'clonotype26', 'clonotype26_consensus_2'],
+                                 ['AAACCTGAGGCTCTTA-1', True, 'AAACCTGAGGCTCTTA-1_contig_2', True, 518, 'TRA', 'TCRAV01-02*01', 'TRBD1', 'TCRAJ33*01', 'TRAC', True, True, 'CAVKDSNYQLIW', 'TGTGCTGTGAAGGATAGCAACTATCAGTTAATCTGG', 2019, 2, 'clonotype26', 'clonotype26_consensus_1'],
+                                 ['AAACCTGAGTGAACGC-1', True, 'AAACCTGAGTGAACGC-1_contig_1', True, 674, 'TRB', 'TCRBV02*01', 'TRBD1', 'TCRBJ01-02*01', 'TRBC1', True, True, 'CASNQGLNYGYTF', 'TGTGCCAGCAATCAGGGCCTTAACTATGGCTACACCTTC', 3002, 6, 'clonotype81', 'clonotype81_consensus_2']],
+                                columns=['barcode', 'is_cell', 'contig_id',
+                                         'high_confidence', 'length', 'chain',
+                                         'v_resolved', 'd_gene', 'j_resolved', 'c_gene',
+                                         'full_length', 'productive', 'cdr3_amino_acid',
+                                         'cdr3_nt', 'reads', 'umis',
+                                         'raw_clonotype_id', 'raw_consensus_id'])
 
-@pytest.mark.parametrize('df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols,out', [
+
+@pytest.mark.parametrize('df, frm, to, rename_cols, frm_cols, out', [
     # 10X <-> Adaptive
-    (tenx_df,'tenx','adaptive',None,False,True,adapt_df),
-    (tenx_df,'tenx','adaptive_v2',None,False,True,adapt_v2_df),
-    (adapt_df,'adaptive','tenx',None,False,True,tenx_df),
-    (adapt_v2_df,'adaptive_v2','tenx',None,False,True,tenx_df),
+    (tenx_df, 'tenx', 'adaptive', True, None, adapt_df),
+    (tenx_df, 'tenx', 'adaptive_v2', True, None, adapt_v2_df),
+    (adapt_df, 'adaptive', 'tenx', True, None, tenx_df),
+    (adapt_v2_df, 'adaptive_v2', 'tenx', True, None, tenx_df),
     # Adaptive <-> IMGT
-    (adapt_df,'adaptive','imgt',None,False,True,imgt_df),
-    (adapt_v2_df,'adaptive_v2','imgt',None,False,True,imgt_df),
-    (imgt_df,'imgt','adaptive',None,False,True,adapt_df),
-    (imgt_df,'imgt','adaptive_v2',None,False,True,adapt_v2_df),
+    (adapt_df, 'adaptive', 'imgt', True, None, imgt_df),
+    (adapt_v2_df, 'adaptive_v2', 'imgt', True, None, imgt_df),
+    (imgt_df, 'imgt', 'adaptive', True, None, adapt_df),
+    (imgt_df, 'imgt', 'adaptive_v2', True, None, adapt_v2_df),
     # IMGT <-> 10X
-    (tenx_df,'tenx','imgt',None,False,True,imgt_df),
-    (imgt_df,'imgt','tenx',None,False,True,tenx_df),
+    (tenx_df, 'tenx', 'imgt', True, None, imgt_df),
+    (imgt_df, 'imgt', 'tenx', True, None, tenx_df),
     # Custom column names
-    (custom_df,'imgt','tenx',['myV', 'myJ', 'myCDR3'],False,True,tenx_df),
+    (custom_df, 'imgt', 'tenx', True, ['myV', 'myJ', 'myCDR3'], tenx_df),
     # Adaptive V2 format but user inputs "adaptive"
-    (adapt_v2_df,'adaptive','tenx',None,False,True,tenx_df),
-    # Extract TCRs only
-    (tenx_df,'tenx','adaptive',None,True,False,extract_df)])
-def test_convert_tcr(df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols,out):
-    result = tcrconvert.convert_tcr(df,fmt_from,fmt_to,cols_use,extract_tcr,convert_cols)
+    (adapt_v2_df, 'adaptive', 'tenx', True, None, tenx_df)])
+def test_convert_df(df, frm, to, rename_cols, frm_cols, out):
+    result = tcrconvert.convert_df(df, frm, to, rename_cols, frm_cols)
     pd.testing.assert_frame_equal(result, out)
+
+
+def test_load_convert():
+    f = str(files('tcrconvert') / 'examples' / 'example_human_10x.csv')
+    result = tcrconvert.load_convert(f, frm='tenx', to='adaptive')
+    pd.testing.assert_frame_equal(result, example_adaptive)
