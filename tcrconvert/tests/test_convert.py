3f6b77155859414b2f24229f186c7c720aed555b
Emma Bishop <bishemma1@gmail.com>
2024-09-10 17:04:29 -0700

    Update test data to reflect C genes not in Adaptive

--- tcrconvert/tests/test_convert.py
+++ tcrconvert/tests/test_convert.py
@@ -2,74 +2,79 @@ import pytest
 import pandas as pd
 import tcrconvert
 
-# TODO: Properly test C genes, gamma J genes (these are species-specific)
-
 tenx_df = pd.DataFrame({'v_gene': ['TRAV12-1', 'TRBV16', 'TRGV3', 'TRDV3'],
                         'd_gene': [pd.NA, 'TRBD2', pd.NA, 'TRDD2'],
-                        'j_gene': ['TRAJ12', 'TRBJ2-1', 'TRGJ1', 'TRDJ2'],
+                        'j_gene': ['TRAJ12', 'TRBJ2-1', pd.NA, 'TRDJ2'],
+                        'c_gene': ['TRAC', 'TRBC1', 'TRGC1', pd.NA],
                         'cdr3': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 imgt_df = pd.DataFrame({'v_gene': ['TRAV12-1*01', 'TRBV16*02', 'TRGV3*01', 'TRDV3*01'],
                         'd_gene': [pd.NA, 'TRBD2*01', pd.NA, 'TRDD2*01'],
-                        'j_gene': ['TRAJ12*01', 'TRBJ2-1*01', 'TRGJ1*01', 'TRDJ2*01'],
+                        'j_gene': ['TRAJ12*01', 'TRBJ2-1*01', pd.NA, 'TRDJ2*01'],
+                        'c_gene': ['TRAC*01', 'TRBC1*01', 'TRGC1*01', pd.NA],
                         'cdr3': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 adapt_df = pd.DataFrame({'v_resolved': ['TCRAV12-01*01', 'TCRBV16*02', 'TCRGV03*01', 'TCRDV03*01'],
                          'd_resolved': [pd.NA, 'TCRBD02*01', pd.NA,'TCRD0D02*01'],
-                         'j_resolved': ['TCRAJ12*01', 'TCRBJ02-01*01', 'TCRGJ01*01', 'TCRDJ02*01'],
+                         'j_resolved': ['TCRAJ12*01', 'TCRBJ02-01*01', pd.NA, 'TCRDJ02*01'],
                          'cdr3_amino_acid': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 adapt_v2_df = pd.DataFrame({'vMaxResolved': ['TCRAV12-01*01', 'TCRBV16*02', 'TCRGV03*01', 'TCRDV03*01'],
                             'dMaxResolved': [pd.NA, 'TCRBD02*01', pd.NA, 'TCRD0D02*01'],
-                            'jMaxResolved': ['TCRAJ12*01', 'TCRBJ02-01*01', 'TCRGJ01*01', 'TCRDJ02*01'],
+                            'jMaxResolved': ['TCRAJ12*01', 'TCRBJ02-01*01', pd.NA, 'TCRDJ02*01'],
                             'aminoAcid': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 tenx_to_imgt_df = pd.DataFrame({'v_gene': ['TRAV12-1*01', 'TRBV16*01', 'TRGV3*01','TRDV3*01'],
                                 'd_gene': [pd.NA, 'TRBD2*01', pd.NA, 'TRDD2*01'],
-                                'j_gene': ['TRAJ12*01', 'TRBJ2-1*01', 'TRGJ1*01', 'TRDJ2*01'],
+                                'j_gene': ['TRAJ12*01', 'TRBJ2-1*01', pd.NA, 'TRDJ2*01'],
+                                'c_gene': ['TRAC*01', 'TRBC1*01', 'TRGC1*01', pd.NA],
                                 'cdr3': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 tenx_to_adapt_df = pd.DataFrame({'v_gene': ['TCRAV12-01*01', 'TCRBV16*01', 'TCRGV03*01', 'TCRDV03*01'],
                                  'd_gene': [pd.NA, 'TCRBD02*01', pd.NA, 'TCRD0D02*01'],
-                                 'j_gene': ['TCRAJ12*01', 'TCRBJ02-01*01', 'TCRGJ01*01', 'TCRDJ02*01'],
+                                 'j_gene': ['TCRAJ12*01', 'TCRBJ02-01*01', pd.NA, 'TCRDJ02*01'],
+                                 'c_gene': [pd.NA, pd.NA, pd.NA, pd.NA],
                                  'cdr3': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 imgt_to_adapt_df = pd.DataFrame({'v_gene': ['TCRAV12-01*01', 'TCRBV16*02', 'TCRGV03*01', 'TCRDV03*01'],
                                  'd_gene': [pd.NA, 'TCRBD02*01', pd.NA, 'TCRD0D02*01'],
-                                 'j_gene': ['TCRAJ12*01', 'TCRBJ02-01*01', 'TCRGJ01*01', 'TCRDJ02*01'],
+                                 'j_gene': ['TCRAJ12*01', 'TCRBJ02-01*01', pd.NA, 'TCRDJ02*01'],
+                                 'c_gene': [pd.NA, pd.NA, pd.NA, pd.NA],
                                  'cdr3': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 adapt_to_tenx_df = pd.DataFrame({'v_resolved': ['TRAV12-1', 'TRBV16', 'TRGV3', 'TRDV3'],
                                  'd_resolved': [pd.NA, 'TRBD2', pd.NA, 'TRDD2'],
-                                 'j_resolved': ['TRAJ12', 'TRBJ2-1', 'TRGJ1', 'TRDJ2'],
+                                 'j_resolved': ['TRAJ12', 'TRBJ2-1', pd.NA, 'TRDJ2'],
                                  'cdr3_amino_acid': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 adapt_to_imgt_df = pd.DataFrame({'v_resolved': ['TRAV12-1*01', 'TRBV16*02', 'TRGV3*01', 'TRDV3*01'],
                                  'd_resolved': [pd.NA, 'TRBD2*01', pd.NA, 'TRDD2*01'],
-                                 'j_resolved': ['TRAJ12*01', 'TRBJ2-1*01', 'TRGJ1*01', 'TRDJ2*01'],
+                                 'j_resolved': ['TRAJ12*01', 'TRBJ2-1*01', pd.NA, 'TRDJ2*01'],
                                  'cdr3_amino_acid': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 adaptv2_to_tenx_df = pd.DataFrame({'vMaxResolved': ['TRAV12-1', 'TRBV16', 'TRGV3', 'TRDV3'],
                                    'dMaxResolved': [pd.NA, 'TRBD2', pd.NA, 'TRDD2'],
-                                   'jMaxResolved': ['TRAJ12', 'TRBJ2-1', 'TRGJ1', 'TRDJ2'],
+                                   'jMaxResolved': ['TRAJ12', 'TRBJ2-1', pd.NA, 'TRDJ2'],
                                    'aminoAcid': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 adaptv2_to_imgt_df = pd.DataFrame({'vMaxResolved': ['TRAV12-1*01', 'TRBV16*02', 'TRGV3*01', 'TRDV3*01'],
                                    'dMaxResolved': [pd.NA, 'TRBD2*01', pd.NA, 'TRDD2*01'],
-                                   'jMaxResolved': ['TRAJ12*01', 'TRBJ2-1*01', 'TRGJ1*01', 'TRDJ2*01'],
+                                   'jMaxResolved': ['TRAJ12*01', 'TRBJ2-1*01', pd.NA, 'TRDJ2*01'],
                                    'aminoAcid': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
  
 custom_df = pd.DataFrame({'myV': ['TRAV12-1*01', 'TRBV16*02', 'TRGV3*01', 'TRDV3*01'],
                           'myD': [pd.NA, 'TRBD2*01', pd.NA, 'TRDD2*01'],
-                          'myJ': ['TRAJ12*01', 'TRBJ2-1*01', 'TRGJ1*01', 'TRDJ2*01'],
+                          'myJ': ['TRAJ12*01', 'TRBJ2-1*01', pd.NA, 'TRDJ2*01'],
+                          'myC': ['TRAC*01', 'TRBC1*01', 'TRGC1*01', pd.NA],
                           'myCDR3': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
 custom_to_tenx_df = pd.DataFrame({'myV': ['TRAV12-1', 'TRBV16', 'TRGV3', 'TRDV3'],
                                   'myD': [pd.NA, 'TRBD2', pd.NA, 'TRDD2'],
-                                  'myJ': ['TRAJ12', 'TRBJ2-1', 'TRGJ1', 'TRDJ2'],
+                                  'myJ': ['TRAJ12', 'TRBJ2-1', pd.NA, 'TRDJ2'],
+                                  'myC': ['TRAC', 'TRBC1', 'TRGC1', pd.NA],
                                   'myCDR3': ['CAVLIF', 'CASSGF', 'CASAAF', 'CASLLF']})
 
-
+# TODO: Test converting from all formats to themselves (e.g. tenx -> tenx)
 @pytest.mark.parametrize('df, frm, to, frm_cols, species, out', [
     # 10X <-> Adaptive
     (tenx_df, 'tenx', 'adaptive', None, 'human', tenx_to_adapt_df),
@@ -85,7 +90,7 @@ custom_to_tenx_df = pd.DataFrame({'myV': ['TRAV12-1', 'TRBV16', 'TRGV3', 'TRDV3'
     (adapt_df, 'adaptive', 'imgt', None, 'human', adapt_to_imgt_df),
     (adapt_v2_df, 'adaptivev2', 'imgt', None, 'human', adaptv2_to_imgt_df),
     # Custom column names
-    (custom_df, 'imgt', 'tenx', ['myV', 'myD', 'myJ', 'myCDR3'], 'human', custom_to_tenx_df),
+    (custom_df, 'imgt', 'tenx', ['myV', 'myD', 'myJ', 'myC'], 'human', custom_to_tenx_df),
     # MOUSE
     (tenx_df, 'tenx', 'adaptive', None, 'mouse', tenx_to_adapt_df),
     (tenx_df, 'tenx', 'adaptivev2', None, 'mouse', tenx_to_adapt_df),
@@ -97,7 +102,7 @@ custom_to_tenx_df = pd.DataFrame({'myV': ['TRAV12-1', 'TRBV16', 'TRGV3', 'TRDV3'
     (imgt_df, 'imgt', 'adaptivev2', None, 'mouse', imgt_to_adapt_df),
     (adapt_df, 'adaptive', 'imgt', None, 'mouse', adapt_to_imgt_df),
     (adapt_v2_df, 'adaptivev2', 'imgt', None, 'mouse', adaptv2_to_imgt_df),
-    (custom_df, 'imgt', 'tenx', ['myV', 'myD', 'myJ', 'myCDR3'], 'mouse', custom_to_tenx_df),
+    (custom_df, 'imgt', 'tenx', ['myV', 'myD', 'myJ', 'myC'], 'mouse', custom_to_tenx_df),
     # RHESUS MACAQUE
     (tenx_df, 'tenx', 'adaptive', None, 'macaque', tenx_to_adapt_df),
     (tenx_df, 'tenx', 'adaptivev2', None, 'macaque', tenx_to_adapt_df),
@@ -109,9 +114,9 @@ custom_to_tenx_df = pd.DataFrame({'myV': ['TRAV12-1', 'TRBV16', 'TRGV3', 'TRDV3'
     (imgt_df, 'imgt', 'adaptivev2', None, 'macaque', imgt_to_adapt_df),
     (adapt_df, 'adaptive', 'imgt', None, 'macaque', adapt_to_imgt_df),
     (adapt_v2_df, 'adaptivev2', 'imgt', None, 'macaque', adaptv2_to_imgt_df),
-    (custom_df, 'imgt', 'tenx', ['myV', 'myD', 'myJ', 'myCDR3'], 'macaque', custom_to_tenx_df)])
-def test_convert_vdj(df, frm, to, frm_cols, species, out):
-    result = tcrconvert.convert_vdj(df, frm, to, frm_cols, species)
+    (custom_df, 'imgt', 'tenx', ['myV', 'myD', 'myJ', 'myC'], 'macaque', custom_to_tenx_df)])
+def test_convert_gene(df, frm, to, frm_cols, species, out):
+    result = tcrconvert.convert_gene(df, frm, to, frm_cols, species)
     # Standardize the NA values so we can check for equality
     result.fillna('blank', inplace=True)
     out.fillna('blank', inplace=True)
